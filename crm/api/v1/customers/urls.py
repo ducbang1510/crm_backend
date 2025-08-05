85d3b95130ed7c9bb4f19d26986961ad1a8fe7e3
Bang Tran <bangtran146@gmail.com>
2025-08-05 22:35:05 +0700

    Implement API to add contact for customer

--- crm/api/v1/customers/urls.py
+++ crm/api/v1/customers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomerViewSet, ContactPersonViewSet
+from oauth2_provider.urls import base_urlpatterns
 
 router = DefaultRouter()
 router.register(r'', CustomerViewSet)
--- crm/api/v1/customers/views.py
+++ crm/api/v1/customers/views.py
@@ -9,19 +9,47 @@ from .serializers import CustomerSerializer, ContactPersonSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.filter(is_active=True)
+    queryset = Customer.objects.all()
     pagination_class = CustomerPagination
     serializer_class = CustomerSerializer
 
     @action(methods=['get'], detail=True, url_path="contacts")
     def get_contacts(self, request, pk):
-        c = self.get_object()
+        customer = self.get_object()
+
         return Response(
-            ContactPersonSerializer(c.contacts.order_by("-id").all(), many=True, context={"request": self.request}).data,
+            ContactPersonSerializer(customer.contacts.order_by("-id").all(), many=True, context={"request": self.request}).data,
             status=status.HTTP_200_OK)
 
+    @action(methods=['post'], detail=True, url_path="add-contact")
+    def get_contacts(self, request, pk):
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        email = request.data.get('email')
+        phone = request.data.get('phone')
+        gender = request.data.get('gender')
+        position = request.data.get('position')
+        date_of_birth = request.data.get('date_of_birth')
+        is_primary= request.data.get('is_primary')
+
+        if gender is None:
+            gender = ContactPerson.OTHER
+
+        if is_primary is None:
+            is_primary = False
+
+        if first_name and last_name and email and phone and position:
+            contact = ContactPerson.objects.create(first_name=first_name, last_name=last_name, email=email, phone=phone,
+                                                   gender=gender, date_of_birth=date_of_birth, is_primary=is_primary,
+                                                   customer=self.get_object())
+            return Response(ContactPersonSerializer(contact).data,
+                            status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
     def get_queryset(self):
-        customers = Customer.objects.filter(is_active=True)
+        customers = Customer.objects.all()
 
         sort_value = self.request.query_params.get('sort')
         if sort_value is not None:
@@ -34,18 +62,27 @@ class CustomerViewSet(viewsets.ModelViewSet):
             elif sort_value == '4':
                 customers = customers.order_by('created_at')
         else:
-            pass
+            customers = customers.order_by('name')
 
         keyword = self.request.query_params.get('keyword')
         if keyword is not None:
             customers = customers.filter(name__icontains=keyword)
 
+        is_active = self.request.query_params.get('is_active')
+        if is_active is not None:
+            customers = customers.filter(is_active=is_active)
+
         return customers
 
-class ContactPersonViewSet(viewsets.ModelViewSet):
+class ContactPersonViewSet(viewsets.ViewSet, generics.RetrieveUpdateAPIView):
+    queryset = ContactPerson.objects.all()
     serializer_class = ContactPersonSerializer
 
     def get_queryset(self):
-        return ContactPerson.objects.filter(
-            customer_id=self.kwargs['id']
-        )
+        contacts = ContactPerson.objects.all()
+
+        customer_id = self.request.query_params.get('customer_id')
+        if customer_id is not None:
+            contacts = contacts.filter(customer_id=customer_id)
+
+        return contacts
